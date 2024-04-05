55a0467b4222bda12eb44467cc0035d0e16f8647
mrudenco <maxim.rudenco@milchundzucker.de>
2024-04-05 14:09:41 +0200

    bugfix

--- tetris.js
+++ tetris.js
@@ -34,12 +34,24 @@ const audio = document.getElementById("tetris-soundtrack");
 
 
 let I = { name: 'I', shape: [[1, 1, 1, 1]] };
-let J = { name: 'J', shape: [[1, 0, 0], [1, 1, 1]] };
-let L = { name: 'L', shape: [[0, 0, 1], [1, 1, 1]] };
-let O = { name: 'O', shape: [[1, 1], [1, 1]] };
-let S = { name: 'S', shape: [[0, 1, 1], [1, 1, 0]] };
-let T = { name: 'T', shape: [[0, 1, 0], [1, 1, 1]] };
-let Z = { name: 'Z', shape: [[1, 1, 0], [0, 1, 1]] };
+
+let J = { name: 'J', shape: [[1, 0, 0],
+                                                   [1, 1, 1]] };
+
+let L = { name: 'L', shape: [[0, 0, 1],
+                                                   [1, 1, 1]] };
+
+let O = { name: 'O', shape: [[1, 1],
+                                                   [1, 1]] };
+
+let S = { name: 'S', shape: [[0, 1, 1],
+                                                   [1, 1, 0]] };
+
+let T = { name: 'T', shape: [[0, 1, 0],
+                                                   [1, 1, 1]] };
+
+let Z = { name: 'Z', shape: [[1, 1, 0],
+                                                   [0, 1, 1]] };
 const array = [I, J, L, O, S, T, Z];
 
 let gameOver = false;
@@ -104,8 +116,8 @@ addEventListener('keydown', ({key}) => {
             }
             break
         case 'ArrowUp':
-            if ((activeMinos.length === 4) && (startBlockX===(canvas.width/2)+((blocksX*blockDimension)/2)-(activeMinos[0].length*blockDimension))) {
-                startBlockX = startBlockX -blockDimension*3
+            if ((activeMinos.length === 4) && (startBlockX+60>=(canvas.width/2)+((blocksX*blockDimension)/2)-(activeMinos[0].length*blockDimension))) {
+                startBlockX = startBlockX -(blockDimension*3)
                 activeMinos = rotate(activeMinos);
             }else if ((activeMinos.length === 3) && (startBlockX===(canvas.width/2)+((blocksX*blockDimension)/2)-(activeMinos[0].length*blockDimension))) {
                 startBlockX = startBlockX -blockDimension
